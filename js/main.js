91453f0f23f3af8a81ef2ed49eafb427d0099fe8
stasygrams <anastasiaivkhn@gmail.com>
2024-11-01 17:39:08 +0200

    my commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const navBtn = document.querySelector(".nav-icon-btn");
+const navIcon = document.querySelector(".nav-icon");
+const nav = document.querySelector(".header__top-row");
+
+navBtn.onclick = function () {
+  navIcon.classList.toggle("nav-icon--active");
+  nav.classList.toggle("header__top-row--mobile");
+  document.body.classList.toggle("no-scroll");
+};
+
+mask("[data-tel-input]");
+
+// Удаляем '+' если больше ничего не введено, чтобы показать placeholder
+const phoneInputs = document.querySelectorAll("[data-tel-input]");
+phoneInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.value == "+") input.value = "";
+  });
+  input.addEventListener("blur", () => {
+    if (input.value == "+") input.value = "";
+  });
+});
